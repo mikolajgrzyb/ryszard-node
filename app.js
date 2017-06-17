a07360b88b41de1b42a85034855c0700284ee763
Mikołaj Grzyb <mikolaj.grzyb@gmail.com>
2017-06-17 20:10:21 +0200

    Add environmental variables.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+var corsOptions = {
+  origin: 'http://localhost:3001'
+}
+
+app.use(cors())
 
 const pool = require('./lib/db');
 
-app.get('/', function (req, response) {
+app.get('/works', function (req, response) {
   pool.query('SELECT * FROM works', function (err, res) {
     if (err) { return console.error('error running query', err); }
     response.send(JSON.stringify(res.rows))
